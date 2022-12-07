742c1659a11118e5fb67b92f80e57523d1680438
Slava Dobrov <dobrov.slava@yandex.ru>
2022-12-07 19:23:14 +0300

    Added documentation

--- src/main/java/DiplomaProject/Data.java
+++ src/main/java/DiplomaProject/Data.java
@@ -20,12 +20,22 @@ public class Data {
         System.out.println("New data was created...");
     }
 
+    /**
+     * This method request for string from user and put this string in fullString value.
+     */
     public void setFullString() {
         Scanner newScanner = new Scanner(System.in);
         System.out.println("Insert information about person");
         this.fullString = newScanner.nextLine();
     }
 
+    /**
+     * Method checkNumberOfWords return:
+     * -1 if there are too many data in inserted string
+     * -2 if there aren't enough data
+     * 0 if user inserted correct number of data
+     * @return
+     */
     public int checkNumberOfWords() {
         if(this.fullString.split(" ").length > NUMBER) {
             return -1;
@@ -36,6 +46,10 @@ public class Data {
         return 0;
     }
 
+    /**
+     * This method receive result of checking of user's string and inform user about made mistakes
+     * @param resultOfChecking
+     */
     public void explainingError(int resultOfChecking) {
         if(resultOfChecking == -1) {
             System.out.println("User insert too many words");
